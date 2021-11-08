2ede32d5cd8610f10e95aacfade43bb3b61dc240
lin-silas <93635072+lin-silas@users.noreply.github.com>
2021-11-08 11:53:38 +0800

    Create naa_install.sh

---
+++ naa_install.sh
@@ -0,0 +1,18 @@
+#!/bin/busybox ash
+
+. /etc/init.d/tc-functions
+. /var/www/cgi-bin/pcp-functions
+
+useBusybox
+TARGET=`cat /etc/sysconfig/backup_device`
+cd /mnt/$TARGET
+sed -i '/naa_arm64.tcz/d' onboot.lst
+echo naa_arm64.tcz >>onboot.lst
+cd optional
+tce-load -i ca-certificates.tcz
+wget https://raw.githubusercontent.com/lin-silas/pcp-naa/main/naa_arm64.tcz
+
+pcp_write_var_to_config USER_COMMAND_3 "(networkaudiod+%26)%3b+taskset+-p+0x00000008+%24(pgrep+networkaudiod*)"
+echo "Rebooting..."
+sleep 3
+pcp
