9bfa399db72bbcf9af39f82d2faac17e8ddbe622
lin-silas <93635072+lin-silas@users.noreply.github.com>
2021-11-09 11:15:02 +0800

    Update naa.sh

--- naa.sh
+++ naa.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo /usr/local/etc/init.d/squeezelite  stop
+ps ax | awk '/[s]queezelite/ { print $1 }' | xargs sudo kill
 networkaudiod &
 sleep 3
 sudo taskset -p 0x00000008 $(pgrep networkaudiod*)
