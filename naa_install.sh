8c60bf3cb7ee32341f5e24bc0205507123414d9a
lin-silas <93635072+lin-silas@users.noreply.github.com>
2021-11-09 10:55:17 +0800

    Update naa_install.sh

--- naa_install.sh
+++ naa_install.sh
@@ -12,24 +12,12 @@ cd optional
 tce-load -i ca-certificates.tcz
 wget https://raw.githubusercontent.com/lin-silas/pcp-naa/main/naa_arm64.tcz
 
-sed -i '11,$d' /opt/bootlocal.sh
-if [ `grep -c 'naa' /opt/bootlocal.sh` -eq 0 ]
-then
-  cat << 'EOL' >> /opt/bootlocal.sh
-#--- Add by lin-silas
-sudo /usr/local/etc/init.d/squeezelite  stop
-networkaudiod &
-sleep 3
-sudo taskset -p 0x00000008 $(pgrep networkaudiod*)
-#--- Add by lin-silas
-EOL
-fi
-
 # tc home
 cd ~
 wget https://raw.githubusercontent.com/lin-silas/pcp-naa/main/naa.sh
 wget https://raw.githubusercontent.com/lin-silas/pcp-naa/main/sq.sh
 sudo chmod +x *.sh
+pcp_write_var_to_config USER_COMMAND_1 "/home/tc/naa.sh"
 
 echo "Rebooting..."
 sleep 3
